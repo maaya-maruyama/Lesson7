fef6a058f7b64654088f34c2886c558611f00cc8
maaya.maruyama <m.maaya@icloud.com>
2024-01-18 18:50:53 +0900

    Initial Commit

---
+++ Lesson7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson7
+//
+//  Created by 丸山万綾 on 2024/01/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
